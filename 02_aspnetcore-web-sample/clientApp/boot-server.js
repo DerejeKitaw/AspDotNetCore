680d87c83fd1b28e5c516525223c1d2cea968404
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-05-28 01:02:50 -0400

    added date on node file

--- 02_aspnetcore-web-sample/clientApp/boot-server.js
+++ 02_aspnetcore-web-sample/clientApp/boot-server.js
@@ -1,8 +1,10 @@
 const prerendering = require('aspnet-prerendering');
 
-module.exports = prerendering.createServerRenderer(function(param){
+module.exports = prerendering.createServerRenderer(function(params){
     return new Promise (function (resolve, reject){
-        var result = "<h1>Hello world, from JS!</h1>";
+        var result = "<h1>Hello world, from JS!</h1>"
+        + "<p>Current time in node: " + new Date() + "</p>"
+        + "<p>URL: " + params.location.path + "</p>" ;
 
         resolve({ html: result });
     })
